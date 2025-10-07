479fefe27a328e6a2a59b01e110f44365c944875
Kyle Corbett <kyle.corbett@sjsu.edu>
2025-10-07 00:03:52 -0700

    Fix quiver color mapping

--- docs/index.js
+++ docs/index.js
@@ -568,35 +568,13 @@ function render() {
             const hue = ((theta % (2 * Math.PI)) / (2 * Math.PI)) * 360;
             // Full saturation and value
             const rgb = hsvToRgb(hue, 1, 1);
-            // Pack into uint32: 0xffRRGGBB
-            buf32[i] = (0xff << 24) | (rgb[0] << 16) | (rgb[1] << 8) | rgb[2];
+            // Pack into uint32: ABGR format (little-endian)
+            buf32[i] = (0xff << 24) | (rgb[2] << 16) | (rgb[1] << 8) | rgb[0];
         }
         ctx.putImageData(imageData, 0, 0);
     } else {
         // Quiver/arrow visualization
         drawQuiver();
-    }
-
-    // HSV to RGB conversion helper
-    function hsvToRgb(h, s, v) {
-        let c = v * s;
-        let x = c * (1 - Math.abs(((h / 60) % 2) - 1));
-        let m = v - c;
-        let r, g, b;
-        if (h < 60) {
-            r = c; g = x; b = 0;
-        } else if (h < 120) {
-            r = x; g = c; b = 0;
-        } else if (h < 180) {
-            r = 0; g = c; b = x;
-        } else if (h < 240) {
-            r = 0; g = x; b = c;
-        } else if (h < 300) {
-            r = x; g = 0; b = c;
-        } else {
-            r = c; g = 0; b = x;
-        }
-        return [Math.round((r + m) * 255), Math.round((g + m) * 255), Math.round((b + m) * 255)];
     }
 
     // Update plot value and history
@@ -764,7 +742,7 @@ function drawQuiver() {
             ctx.stroke();
             
             // Draw arrow head
-            const angle = theta; // theta is already the angle
+            const angle = theta; // Use the same angle for arrow head
             const headAngle = Math.PI / 6; // 30 degrees
             
             // Left side of arrow head
